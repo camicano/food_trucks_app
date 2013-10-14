1a5a832d7f89239408eb901dfd65ccd1a24ad172
Camila Cano <camilacanog@gmail.com>
2013-10-14 11:29:24 -0400

    add factories

--- spec/models/truck_spec.rb
+++ spec/models/truck_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe Truck do
   it "is invalid without a name" do
   	Factory.build(:contact, firstname: nil).should_not be_valid 
-
   end
   # it "belongs to many foods"
   # it "belongs to one user"
