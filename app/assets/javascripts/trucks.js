6abc48527a7722ff0d60d66888ac7d2a206d62d7
Camila Cano <camilacanog@gmail.com>
2013-10-14 16:07:07 -0400

    google maps finds the costumer's location on the map

--- app/assets/javascripts/trucks.js
+++ app/assets/javascripts/trucks.js
@@ -1,16 +1,54 @@
-var json;
+var latitude,
+	longitude,
+	myLatlng,
+	map;
+
+function geoFindMe() {
+  var output = document.getElementById("out");
+
+  if (!navigator.geolocation){
+    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+    return;
+  }
+
+  function success(position) {
+    latitude  = position.coords.latitude;
+    longitude = position.coords.longitude;
+    initialize(latitude, longitude);
+    output.innerHTML = "<p>Located</p>";
+    myLatlng = new google.maps.LatLng(latitude,longitude);
+    initialize();
+  };
+
+  function error() {
+    output.innerHTML = "Unable to retrieve your location";
+  };
+
+  output.innerHTML = "<p>Locating…</p>";
+
+  navigator.geolocation.getCurrentPosition(success, error);
+}
+
+function initialize() {
+    var mapOptions = {
+        center: myLatlng,
+        zoom: 8,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+map = new google.maps.Map(document.getElementById("map-canvas"),
+    mapOptions);
+
+var marker = new google.maps.Marker({
+    position: myLatlng,
+    map: map,
+    title:"Hello World!"
+});
+
+}
 
 
 
 $(document).ready(function(){
-      function initialize() {
-        var mapOptions = {
-          center: new google.maps.LatLng(-34.397, 150.644),
-          zoom: 8,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(document.getElementById("map-canvas"),
-            mapOptions);
-      }
-      google.maps.event.addDomListener(window, 'load', initialize);
+	geoFindMe();
+    // google.maps.event.addDomListener(window, 'load', initialize);
 });
