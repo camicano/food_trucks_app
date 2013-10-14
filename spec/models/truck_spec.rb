c5d29c6550ba4b254b63e3e8a50c8a31658f84b8
Camila Cano <camilacanog@gmail.com>
2013-10-14 11:25:17 -0400

    testing

--- spec/models/truck_spec.rb
+++ spec/models/truck_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Truck do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is invalid without a name" do
+  	Factory.build(:contact, firstname: nil).should_not be_valid 
+
+  end
+  # it "belongs to many foods"
+  # it "belongs to one user"
+  it "it is invalid without a latitude"
+  it "it is invalid without a longitude"
 end
