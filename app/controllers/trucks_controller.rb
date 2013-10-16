ea2fd705ca1d6c0c844762fefd593d12c22fd2df
Jeivar <jfernadel@gmail.com>
2013-10-16 14:22:46 -0400

    navbar

--- app/controllers/trucks_controller.rb
+++ app/controllers/trucks_controller.rb
@@ -10,12 +10,12 @@ class TrucksController < ApplicationController
 		end	
 	end
 
-	def tweets
-		truck = params[:truck].split('.')
-		selected_truck = Truck.find_by_name(truck[0])
-		@tweet = selected_truck.tweets
-		respond_to do |format|
-			format.json { render :json => @tweet.to_json }
-		end
-	end
+	# def tweets
+	# 	truck = params[:truck].split('.')
+	# 	selected_truck = Truck.find_by_name(truck[0])
+	# 	@tweet = selected_truck.tweets
+	# 	respond_to do |format|
+	# 		format.json { render :json => @tweet.to_json }
+	# 	end
+	# end
 end
